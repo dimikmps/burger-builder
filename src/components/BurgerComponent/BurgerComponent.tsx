7e8c49b6225dc55c1f8d29219396b52e56edd1de
dimikmps <dimitris.kmps@gmail.com>
2024-07-02 19:24:16 +0300

    Finalize burger component

--- src/components/BurgerComponent/BurgerComponent.tsx
+++ src/components/BurgerComponent/BurgerComponent.tsx
@@ -1,3 +1,4 @@
+import { Box } from '@mui/material';
 import { useIngredients } from '../../contexts/IngredientsContext';
 
 const BurgerComponent = (): React.JSX.Element => {
@@ -8,15 +9,43 @@ const BurgerComponent = (): React.JSX.Element => {
    };
 
    return (
-      <div>
-         <img src='https://react-interview.xm.com/img/bun_top.png' alt='bun_top' />
+      <Box my={4} textAlign='center'>
+         <Box
+            component='img'
+            src='https://react-interview.xm.com/img/bun_top.png'
+            alt='bun_top'
+            display='block'
+            margin='auto'
+            sx={{
+               width: { xs: '80%', sm: '60%', md: '50%', lg: '40%' },
+            }}
+         />
          {currentIngredients.map((ingredient, index) => (
-            <div key={index} onClick={() => handleRemoveIngredientFromCurrent(index)}>
-               <img src={`https://react-interview.xm.com/img/${ingredient.src}`} alt={ingredient.name} />
-            </div>
+            <Box
+               key={index}
+               onClick={() => handleRemoveIngredientFromCurrent(index)}
+               component='img'
+               src={`https://react-interview.xm.com/img/${ingredient.src}`}
+               alt={ingredient.name}
+               display='block'
+               margin='auto'
+               sx={{
+                  width: { xs: '80%', sm: '60%', md: '50%', lg: '40%' },
+                  cursor: 'pointer',
+               }}
+            />
          ))}
-         <img src='https://react-interview.xm.com/img/bun_bottom.png' alt='bun_bottom' />
-      </div>
+         <Box
+            component='img'
+            src='https://react-interview.xm.com/img/bun_bottom.png'
+            alt='bun_bottom'
+            display='block'
+            margin='auto'
+            sx={{
+               width: { xs: '80%', sm: '60%', md: '50%', lg: '40%' },
+            }}
+         />{' '}
+      </Box>
    );
 };
 
